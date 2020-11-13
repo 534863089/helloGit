7257e7166588941809607400c8dac067bbad7e37
ShenghuaZhu <534863089@qq.com>
2020-11-13 12:26:38 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author samz.
+ * @create 2020-11-13-12:09
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello Git!");
+    }
+}
